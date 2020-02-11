d3d87045e89bbad70c90f220517727bc7bd40483
RoundBearChoi <roundbeargames@gmail.com>
2020-02-11 22:01:50 +0900

    #13 remove decimals

---
+++ CubeSnakeGame/Assets/RB_Snake/LevelDesign/IntPositions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RBSnake
+{
+    public class IntPositions : MonoBehaviour
+    {
+        [ContextMenu("RemoveDecimals")]
+        void RemoveDecimals()
+        {
+            foreach (Transform child in transform)
+            {
+                Grass grass = child.GetComponent<Grass>();
+                Water water = child.GetComponent<Water>();
+
+                if (grass != null || water != null)
+                {
+                    Debug.Log("removing decimals: " + child.gameObject.name);
+                    child.transform.position = new Vector3(
+                        Mathf.Round(child.transform.position.x),
+                        Mathf.Round(child.transform.position.y),
+                        Mathf.Round(child.transform.position.z));
+                }
+            }
+        }
+    }
+}
