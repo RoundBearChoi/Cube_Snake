a55c8a4ab4888a48bc082363dabe81d076e32800
RoundBearChoi <roundbeargames@gmail.com>
2020-05-29 15:24:46 +0900

    #16 instantiating markers

--- CubeSnakeGame/Assets/RB_Snake/Snake/SnakePlayer.cs
+++ CubeSnakeGame/Assets/RB_Snake/Snake/SnakePlayer.cs
@@ -9,14 +9,21 @@ namespace RBSnake
         public bool IsDead;
         public bool IsDrowning;
 
+        [SerializeField] List<GameObject> Markers = new List<GameObject>();
+        Control control;
+
         private void Awake()
         {
             IsDead = false;
+            control = FindObjectOfType<Control>();
+            Markers.Clear();
         }
 
         private void Update()
         {
             ToggleDeathMenu();
+            InstantiateMarkers();
+            CheckNextBlock();
         }
 
         void ToggleDeathMenu()
@@ -29,6 +36,46 @@ namespace RBSnake
                     {
                         UIManager.Instance.DeathMenu.ShowDeathMenu();
                     }
+                }
+            }
+        }
+
+        void CheckNextBlock()
+        {
+            if (control.KeyPresses.Count > 0)
+            {
+                KeyCode key = control.KeyPresses[0];
+
+                if (key == KeyCode.LeftArrow)
+                {
+
+                }
+                else if (key == KeyCode.RightArrow)
+                {
+
+                }
+                else if (key == KeyCode.UpArrow)
+                {
+
+                }
+                else if (key == KeyCode.DownArrow)
+                {
+
+                }
+            }
+        }
+
+        void InstantiateMarkers()
+        {
+            if (Markers.Count == 0)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    GameObject m = Instantiate(Resources.Load("Marker")) as GameObject;
+                    m.transform.parent = this.transform;
+                    m.transform.localPosition = Vector3.zero;
+                    m.transform.localRotation = Quaternion.identity;
+                    Markers.Add(m);
                 }
             }
         }
